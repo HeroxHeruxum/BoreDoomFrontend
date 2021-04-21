3cfefcc74bce789c6fec4368dd8a37e4b9636f1e
Herox <stanfalkenrich@gmail.com>
2021-04-21 12:59:40 +0200

    added React-Dom dependency, and implemented the Router structure in App.tsx, implemented MainPage.tsx as our landing page, added path calls to the Buttons in GeneralHeader.tsx.

--- boredoom-frontend/src/App.tsx
+++ boredoom-frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import {GeneralHeader} from "./component/GeneralHeader";
+import {MainPage} from "./Pages/MainPage";
+import { createBrowserHistory } from 'history';
+import { Router, Route, Switch } from 'react-router'
 
 function App() {
+    const history = createBrowserHistory();
+
     return (
-        <header>
-            <GeneralHeader/>
-        </header>
+        <Router history={ history }>
+            <Switch>
+                <Route path={"/"} render={() => <MainPage/>}/>
+            </Switch>
+        </Router>
     );
 }
 
