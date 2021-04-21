94ee8c6b27f0ba7560413021df36d9fda62e2a58
Herox <stanfalkenrich@gmail.com>
2021-04-21 12:59:40 +0200

    added React-Dom dependency, and implemented the Router structure in App.tsx, implemented MainPage.tsx as our landing page, added path calls to the Buttons in GeneralHeader.tsx.

---
+++ boredoom-frontend/src/Pages/MainPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {GeneralHeader} from "../component/GeneralHeader";
+
+export function MainPage(){
+    return(
+        <header>
+            <GeneralHeader/>
+        </header>
+    );
+}
--- boredoom-frontend/src/component/GeneralHeader.tsx
+++ boredoom-frontend/src/component/GeneralHeader.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 import Button from '@material-ui/core/Button';
 
 
 export function GeneralHeader(): JSX.Element {
+    let history = useHistory();
+
+    function handleClick() {
+    }
     return (
         <div style={{backgroundColor: "#273661", width: "100%", top: 0, display: "inline-flex"}}>
             <div style={{left:"0px"}}>
                 <Button style={{color: "white", height: "50px"}}><h2>BoreDoom</h2></Button>
             </div>
             <div style={{position:"absolute",right: "0px"}}>
-                <Button style={{color: "white", height: "50px"}}>Fragenkatalog</Button>
-                <Button style={{color: "white", height: "50px"}}>Glückstreffer</Button>
-                <Button style={{color: "white", height: "50px"}}>Merkliste</Button>
-                <Button style={{color: "white", height: "50px"}}>Login</Button>
+                <Button style={{color: "white", height: "50px"}} onClick={()=>{history.push('/fragenkatalog')}}>Fragenkatalog</Button>
+                <Button style={{color: "white", height: "50px"}} onClick={()=>{history.push('/glueckstreffer')}}>Glückstreffer</Button>
+                <Button style={{color: "white", height: "50px"}} onClick={()=>{history.push('/merkliste')}}>Merkliste</Button>
+                <Button style={{color: "white", height: "50px"}} onClick={()=>{history.push('/login')}}>Login</Button>
             </div>
         </div>
     );
