366a47d58b7bf44755188b04e824f40b54c59311
Herox <stanfalkenrich@gmail.com>
2021-05-18 08:48:35 +0200

    fixed some errors

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,6 +2,7 @@ import "./header.scss";
 import { useCallback } from "react";
 import { Button } from "../button/button";
 import { useHistory } from "react-router-dom";
+import React from "react";
 
 
 export function Header() {
@@ -17,12 +18,13 @@ export function Header() {
                 <div style={{fontSize: "30px", marginRight: "18px", cursor: "pointer"}}>BoreDoom</div>
                 <Button type="header" title="Fragenkatalog" onClick={getOnClickMenuButton("questions")}/>
                 <Button type="header" title="Glückstreffer" onClick={getOnClickMenuButton("luckystrike")}/>
-                <Button type="header" title="Merkliste" onClick={getOnClickMenuButton("bookmarks")}/>
+                <Button type="header" title="Merkliste" onClick={getOnClickMenuButton("bookmark" +
+                    "s")}/>
                 <Button type="header" title="Kontakt" onClick={getOnClickMenuButton("contact")}/>
             </div>
             <div className="headerRight">
                 <Button type="header" title="Anmelden" onClick={getOnClickMenuButton("login")}/>
             </div>
         </div>
-    )
+    );
 }
